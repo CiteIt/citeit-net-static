8c5939ea866d0218d1d4c2e5f9d3aecf5c1d502c
Tim Langeman <timlangeman@gmail.com>
2021-03-05 21:17:49 -0500

    update OrganicTabs: Better Linkable Tabs
      * update OrganicTabs: Better Linkable Tabs
         - https://css-tricks.com/better-linkable-tabs/

--- assets/js/OrganicTabs/js/organictabs.jquery.js
+++ assets/js/OrganicTabs/js/organictabs.jquery.js
@@ -1,16 +1,25 @@
+// IIFE
 (function($) {
 
+		// Define Plugin
     $.organicTabs = function(el, options) {
     
+    		// JavaScript native version of this
         var base = this;
+        
+        // jQuery version of this
         base.$el = $(el);
+        
+        // Navigation for current selector passed to plugin
         base.$nav = base.$el.find(".nav");
-                
+        
+        // Runs once when plugin called       
         base.init = function() {
         
+        		// Pull in arguments
             base.options = $.extend({},$.organicTabs.defaultOptions, options);
-            
-            // Accessible hiding fix
+                        
+            // Accessible hiding fix (hmmm, re-look at this, screen readers still run JS)
             $(".hide").css({
                 "position": "relative",
                 "top": 0,
@@ -18,7 +27,11 @@
                 "display": "none"
             }); 
             
-            base.$nav.delegate("li > a", "click", function() {
+            // When navigation tab is clicked...
+            base.$nav.delegate("a", "click", function(e) {
+            
+            		// no hash links
+            		e.preventDefault();
             
                 // Figure out current list via CSS class
                 var curList = base.$el.find("a.current").attr("href").substring(1),
@@ -32,7 +45,7 @@
                 // Set outer wrapper height to (static) height of current inner list
                     $allListWrap = base.$el.find(".list-wrap"),
                     curListHeight = $allListWrap.height();
-                $allListWrap.height(curListHeight);
+                		$allListWrap.height(curListHeight);
                                         
                 if ((listID != curList) && ( base.$el.find(":animated").length == 0)) {
                                             
@@ -46,28 +59,54 @@
                         var newHeight = base.$el.find("#"+listID).height();
                         $allListWrap.animate({
                             height: newHeight
-                        });
+                        }, base.options.speed);
                         
                         // Remove highlighting - Add to just-clicked tab
                         base.$el.find(".nav li a").removeClass("current");
                         $newList.addClass("current");
-                            
+                        
+												// Change window location to add URL params
+												if (window.history && history.pushState) {
+												  // NOTE: doesn't take into account existing params
+													history.replaceState("", "", "?" + base.options.param + "=" + listID);
+												}    
                     });
                     
                 }   
-                
-                // Don't behave like a regular link
-                // Stop propegation and bubbling
-                return false;
+
             });
+            
+            var queryString = {};
+						window.location.href.replace(
+						    new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+						    function($0, $1, $2, $3) { queryString[$1] = $3; }
+						);
+            
+            if (queryString[base.options.param]) {
+            
+            	var tab = $("a[href='#" + queryString[base.options.param] + "']");
+            
+							tab
+								.closest(".nav")
+								.find("a")
+								.removeClass("current")
+								.end()
+								.next(".list-wrap")
+								.find("ul")
+								.hide();
+							tab.addClass("current");
+							$("#" + queryString[base.options.param]).show();
+			            
+			        };            
             
         };
         base.init();
     };
     
-    $.organicTabs.defaultOptions = {
-        "speed": 300
-    };
+		$.organicTabs.defaultOptions = {
+		    "speed": 300,
+		    "param": "tab"
+		};
     
     $.fn.organicTabs = function(options) {
         return this.each(function() {
